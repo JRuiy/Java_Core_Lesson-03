32610a9c5ff6cd1ee19e71f320f274bbc7411c8c
VadymKhodorovskyi <vadimhodor@gmail.com>
2020-11-30 11:02:13 +0200

    Change

--- Lesson_01/src/ua/lviv/lgs/Aplication_01.java
+++ Lesson_01/src/ua/lviv/lgs/Aplication_01.java
@@ -13,29 +13,30 @@ public class Aplication_01 {
 		long g = 524l;
 		short h = 500;
 		
+		System.out.println("РўРµСЃС‚СѓРІР°РЅРЅСЏ ");
 
 		
-		System.out.println("Максимально і  мінімального значеня в Boolean немає ");
-		System.out.println("Максимальне значення Byte = "+ Byte.MAX_VALUE);
-		System.out.println("Мінімальне значення Byte = "+ Byte.MIN_VALUE);
+		System.out.println("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ  пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ Boolean пїЅпїЅпїЅпїЅ ");
+		System.out.println("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ Byte = "+ Byte.MAX_VALUE);
+		System.out.println("МіпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ Byte = "+ Byte.MIN_VALUE);
 		System.out.println();
-		System.out.println("Максимальне значення Char = "+ Character.MAX_VALUE);
-		System.out.println("Мінімальне значення Char = "+ Character.MIN_VALUE);
+		System.out.println("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ Char = "+ Character.MAX_VALUE);
+		System.out.println("МіпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ Char = "+ Character.MIN_VALUE);
 		System.out.println();
-		System.out.println("Максимальне значення Double = "+ Double.MAX_VALUE);
-		System.out.println("Мінімальне значення Double = "+ Double.MIN_VALUE);
+		System.out.println("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ Double = "+ Double.MAX_VALUE);
+		System.out.println("МіпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ Double = "+ Double.MIN_VALUE);
 		System.out.println();
-		System.out.println("Максимальне значення Float = "+ Float.MAX_VALUE);
-		System.out.println("Мінімальне значення Float = "+ Float.MIN_VALUE);
+		System.out.println("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ Float = "+ Float.MAX_VALUE);
+		System.out.println("МіпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ Float = "+ Float.MIN_VALUE);
 		System.out.println();
-		System.out.println("Максимальне значення Integer = "+ Integer.MAX_VALUE);
-		System.out.println("Мінімальне значення Integer = "+ Integer.MIN_VALUE);
+		System.out.println("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ Integer = "+ Integer.MAX_VALUE);
+		System.out.println("МіпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ Integer = "+ Integer.MIN_VALUE);
 		System.out.println();
-		System.out.println("Максимальне значення Long = "+ Long.MAX_VALUE);
-		System.out.println("Мінімальне значення Long = "+ Long.MIN_VALUE);
+		System.out.println("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ Long = "+ Long.MAX_VALUE);
+		System.out.println("МіпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ Long = "+ Long.MIN_VALUE);
 		System.out.println();
-		System.out.println("Максимальне значення Short = "+ Short.MAX_VALUE);
-		System.out.println("Мінімальне значення Short = "+ Short.MIN_VALUE);
+		System.out.println("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ Short = "+ Short.MAX_VALUE);
+		System.out.println("МіпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ Short = "+ Short.MIN_VALUE);
 		
 	}
 	
